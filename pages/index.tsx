cb5405339aaf9fccf579069ce82557a21cd46dd1
NecroBread <mariovyordanov@gmail.com>
2022-09-30 09:33:07 +0300

    add tailwind and daisyui

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head'
+
+const Home = () => {
+    return (
+        <>
+            <Head>
+                <title>Next Shop</title>
+            </Head>
+            <main>
+                <h1>Next  Shop</h1>
+            </main>
+        </>
+    )
+}
+
+export default Home;
