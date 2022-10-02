9a39a5962a48a9b9c1d3667d7311cda03190fdd4
NecroBread <mariovyordanov@gmail.com>
2022-10-02 14:33:48 +0300

    create: sign up component
    and adjust sign in styles

--- client/pages/sign-in/index.tsx
+++ client/pages/sign-in/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { FormEvent } from 'react';
 import PageHeading from '../../components/common/heading';
@@ -21,40 +22,52 @@ const SignIn = () => {
     }
 
     return (
-        <div className='flex justify-center items-center'>
-            <form onSubmit={handleSubmit} className='p-8 w-full sm:w-96  form-control gap-2'>
-                <PageHeading>Sign In</PageHeading>
-                <div className="form-control w-full max-w-xs">
-                    <label htmlFor='email' className="label">
-                        <span className="label-text">Email</span>
-                    </label>
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Type here"
-                        className="input input-bordered w-full max-w-xs"
-                        required={true}
-                    />
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Password</span>
-                    </label>
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Choose secure password"
-                        className="input input-bordered w-full max-w-xs"
-                        required={true}
-                    />
-                </div>
-                {signInIsError && <span className='text-error-content text-center block p-1 bg-error rounded-lg'>Invalid credentials</span>}
-                <button
-                    disabled={signInIsLoading}
-                    type='submit'
-                    className={`btn btn-primary mt-4 ${signInIsLoading && 'loading'}`}
-                >Submit</button>
-            </form>
+        <div className='flex flex-col justify-center align-middle sm:items-center'>
+            <section>
+                <form onSubmit={handleSubmit} className='p-8 w-full sm:w-96 form-control gap-2'>
+                    <PageHeading>Sign In</PageHeading>
+                    <div className="form-control w-full">
+                        <label htmlFor='email' className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            className="input input-bordered w-full"
+                            required={true}
+                        />
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            className="input input-bordered w-full"
+                            required={true}
+                        />
+                    </div>
+                    {signInIsError && <span className='text-error-content text-center block p-1 bg-error rounded-lg'>Invalid credentials</span>}
+                    <button
+                        disabled={signInIsLoading}
+                        type='submit'
+                        className={`btn btn-primary mt-4 ${signInIsLoading && 'loading'}`}
+                    >Submit</button>
+                </form>
+            </section>
+            <section className='min-w-[320px]'>
+                <div className="divider">Don't have an account?</div>
+            </section>
+            <section>
+                <Link href="/sign-up">
+                    <a className='btn btn-primary btn-outline w-full'>
+                        Create account
+                    </a>
+                </Link>
+            </section>
         </div>
     )
 }
--- client/pages/sign-up/index.tsx
+++ client/pages/sign-up/index.tsx
@@ -1,37 +1,108 @@
-import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { FormEvent } from 'react';
+import PageHeading from '../../components/common/heading';
+import { useSignIn } from '../../hooks/useSignIn';
 
-const SignUp = () => {
+const SignIn = () => {
+    const router = useRouter();
+    const { signIn, signInIsLoading, signInIsError } = useSignIn();
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const username = formData.get('username') as string;
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+        const rePassword = formData.get('rePassword') as string;
+
+        if (password !== rePassword) {
+            return undefined; // TODO add notifications
+        }
+
+        // TODO Add Sign Up hook
+    }
+
     return (
-        <>
-            <form>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Username</span>
-                    </label>
-                    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Email</span>
-                    </label>
-                    <input type="email" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Password</span>
-                    </label>
-                    <input type="password" placeholder="Choose secure password" className="input input-bordered w-full max-w-xs" />
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Repeat Password</span>
-                    </label>
-                    <input type="password" placeholder="Repeat password" className="input input-bordered w-full max-w-xs" />
-                </div>
-                <button>Submit</button>
-            </form>
-        </>
+        <div className='flex flex-col justify-center align-middle sm:items-center'>
+            <section>
+                <form onSubmit={handleSubmit} className='p-8 w-full sm:w-96 form-control gap-2'>
+                    <PageHeading>Sign Up</PageHeading>
+                    <div className="form-control w-full">
+                        <label htmlFor='username' className="label">
+                            <span className="label-text">Username</span>
+                        </label>
+                        <input
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            className="input input-bordered w-full"
+                            required={true}
+                            minLength={3}
+                            maxLength={30}
+                        />
+                    </div>
+                    <div className="form-control w-full">
+                        <label htmlFor='email' className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            className="input input-bordered w-full"
+                            required={true}
+                        />
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            className="input input-bordered w-full"
+                            required={true}
+                            minLength={6}
+                            maxLength={100}
+                        />
+                    </div>
+                    <div className="form-control w-full">
+                        <label className="label">
+                            <span className="label-text">Repeat Password</span>
+                        </label>
+                        <input
+                            type="password"
+                            name="rePassword"
+                            placeholder="Repeat Password"
+                            className="input input-bordered w-full"
+                            required={true}
+                            minLength={6}
+                            maxLength={100}
+                        />
+                    </div>
+                    {signInIsError && <span className='text-error-content text-center block p-1 bg-error rounded-lg'>Invalid credentials</span>}
+                    <button
+                        disabled={signInIsLoading}
+                        type='submit'
+                        className={`btn btn-primary mt-4 ${signInIsLoading && 'loading'}`}
+                    >Submit</button>
+                </form>
+            </section>
+            <section className='min-w-[320px]'>
+                <div className="divider">Already have an account?</div>
+            </section>
+            <section>
+                <Link href="/sign-in">
+                    <a className='btn btn-primary btn-outline w-full'>
+                        Sign In
+                    </a>
+                </Link>
+            </section>
+        </div>
     )
 }
 
-export default SignUp;
+export default SignIn;
